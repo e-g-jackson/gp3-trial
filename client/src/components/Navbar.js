bbf104ddfd2a0566d200fcf157f0f0e92941a105
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-03-01 02:25:13 -0600

    added linking

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Navbar.css';
 
 class Navbar extends React.Component {
@@ -7,7 +8,7 @@ class Navbar extends React.Component {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item nav-item-1">
+                        {/* <li className="nav-item nav-item-1">
                             <a className="nav-link" href="/profile">Profile</a>
                         </li>
                         <li className="nav-item">
@@ -18,6 +19,34 @@ class Navbar extends React.Component {
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/progress">Progress</a>
+                        </li> */}
+                        <li className="nav-item nav-item-1">
+                            <Link to="/profile">
+                                <div className = "nav-link">   
+                                    Profile
+                                </div>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/food">
+                                <div className = "nav-link">   
+                                    Food Tracker
+                                </div>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/types">
+                                <div className = "nav-link">   
+                                    FoodTypes
+                                </div>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/progress">
+                                <div className = "nav-link">   
+                                    Progress
+                                </div>
+                            </Link>
                         </li>
                     </ul>
                 </div>
