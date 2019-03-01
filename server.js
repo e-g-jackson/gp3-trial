ed977aca6eb0d0c1851b833cd96ff7afc2bccb42
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-28 23:20:36 -0600

    retry

--- server.js
+++ server.js
@@ -41,13 +41,13 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 mongoose.connect(MONGODB_URI);
 
