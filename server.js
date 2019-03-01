253c7377d95c70d8f9a442d068d5fe2e2aed5fcf
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-28 22:24:45 -0600

    adjusting cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(logger("dev"));
 //CORS
-var whitelist = ['https://wellness-tracker-app.herokuapp.com/', 'http://localhost:3000']
+var whitelist = [process.env.NODE_ENV, 'http://localhost:3000']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -32,7 +32,9 @@ app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
 
