f6c6a910ca4d47d482fb31b3b4895b5facf9bcf4
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-28 23:18:50 -0600

    cors changes

--- server.js
+++ server.js
@@ -15,8 +15,19 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/WellnessTracke
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(logger("dev"));
+
 //CORS
-var whitelist = [process.env.NODE_ENV, 'https://wellness-tracker-app.herokuapp.com/', 'http://localhost:3000']
+const production  = 'https://wellness-tracker-app.herokuapp.com/';
+const development = 'http://localhost:3001/';
+var url;
+
+if (process.env.NODE_ENV === "production"){
+    url = production;
+} else if (process.env.NODE_ENV === "development"){
+    url = development;
+}
+
+var whitelist = [url]
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -30,13 +41,13 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+//   });
 
 mongoose.connect(MONGODB_URI);
 
